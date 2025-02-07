865b654237b2933fd3a83b0bb4a72ed91c748fa2
marioBytes <mario.bytes92@gmail.com>
2025-02-06 19:00:28 -0700

    Getting error if react wasn't imported

--- client/src/features/Builds/components/BuildRow.tsx
+++ client/src/features/Builds/components/BuildRow.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Check, Edit2, Trash2, X } from 'lucide-react';
 
 import { Build, Grade, Priority, Status } from '../types';
--- client/src/features/Download/components/Download.tsx
+++ client/src/features/Download/components/Download.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Download as DownloadIcon } from 'lucide-react';
 
 import Select from '../../../components/Select';
